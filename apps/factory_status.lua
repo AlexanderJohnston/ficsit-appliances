c827e8f093ea505615235f60069e0ff16cb334af
Zoltán Nagy <abesto@abesto.net>
2022-01-19 21:47:10 +0000

    Refactors, factory_status WIP

--- apps/factory_status.lua
+++ apps/factory_status.lua
@@ -1,5 +1,12 @@
 local class = Deps("kikito/middleclass:middleclass", "v4.1.1")
-local inspect = Deps("kikito/inspect.lua:inspect", "v3.1.2")
+
+local hw = Deps("lib/hw")
+local TablePrinter = Deps("lib/table_printer")
+local colors = Deps("lib/colors")
+
+CONFIG = CONFIG or {
+    main_display = "MainScreen"
+}
 
 local EntryBuilder = class("EntryBuilder")
 function EntryBuilder:initialize(o)
@@ -47,9 +54,20 @@ function EntryBuilder:build()
     for _, productivity in pairs(self.productivities) do
         sum_productivity = sum_productivity + productivity
     end
+
+    local worst_status = "Working"
+    for _, status in pairs(self.statuses) do
+        if status == "Starved" and worst_status ~= "Starved" then
+            worst_status = status
+        elseif status == "BackedUp" and worst_status == "Working" then
+            worst_status = status
+        end
+    end
+
     return {
         productivity = sum_productivity / #self.productivities,
-        statuses = self.statuses
+        statuses = self.statuses,
+        worst_status = worst_status
     }
 end
 
@@ -73,12 +91,43 @@ function SnapshotBuilder:build()
 end
 
 local function main()
+    local gpu = hw.gpu()
+    local main_display = component.proxy(CONFIG.main_display)
+    gpu:bindScreen(main_display)
+
+    local table_printer = TablePrinter:new{
+        headings = {"Recipe", "Productivity", "Status"}
+    }
+
     local builder = SnapshotBuilder:new()
     local factories = component.proxy(component.findComponent(findClass("Factory")))
     for _, factory in pairs(factories) do
         builder:record(factory:getRecipe().name, factory.productivity, factory:getInventories())
     end
-    print(inspect(builder:build()))
+
+    local entries = builder:build()
+    for name, entry in pairs(entries) do
+        local color
+        if entry.productivity > 0.9 then
+            color = colors.GREEN
+        elseif entry.worst_status == "BackedUp" then
+            color = colors.YELLOW
+        elseif entry.worst_status == "Starved" then
+            color = colors.RED
+        else
+            color = colors.WHITE
+        end
+
+        local status_strs = {}
+        for status, count in pairs(entry.statuses) do
+            table.insert(status_strs, string.format("%s: %d", status, count))
+        end
+        local status_str = table.concat(status_strs, " | ")
+        table_printer:insert(color, {name, entry.productivity, status_str})
+    end
+
+    table_printer:sort()
+    table_printer:print(nil, gpu)
 end
 
 return main
--- apps/storage_display.lua
+++ apps/storage_display.lua
@@ -1,17 +1,11 @@
 local fs = Deps("lib/fs")
 local binser = Deps("bakpakin/binser:binser", "0.0-8")
 local class = Deps("kikito/middleclass:middleclass", "v4.1.1")
-local time = Deps("lib/time")
-local shellsort = Deps("third_party/shellsort")
-local hw = Deps("lib/hw")
 
-BLACK = {0, 0, 0, 1}
-WHITE = {1, 1, 1, 1}
-GRAY30 = {0.3, 0.3, 0.3, 1}
-GRAY50 = {0.5, 0.5, 0.5, 1}
-GREEN = {0, 1, 0, 1}
-RED = {1, 0, 0, 1}
-YELLOW = {1, 1, 0, 1}
+local time = Deps("lib/time")
+local hw = Deps("lib/hw")
+local TablePrinter = Deps("lib/table_printer")
+local colors = Deps("lib/colors")
 
 CONFIG = CONFIG or CONFIG {
     main_display = "7FC05BBE4B398CD7430CFDAF66DDCC17",
@@ -222,97 +216,11 @@ local function count_items(db, container)
     end
 end
 
-TablePrinter = class("TablePrinter")
-function TablePrinter:initialize(o)
-    self.headings = {
-        cells = o.headings
-    }
-    self.rows = {}
-    self.rowcolors = {}
-    self.widths = {}
-
-    for i, heading in pairs(self.headings.cells) do
-        self.widths[i] = #heading
-    end
-end
-
-function TablePrinter:sort()
-    shellsort(self.rows, function(a, b)
-        return a.cells[1] < b.cells[1]
-    end)
-end
-
-function TablePrinter:insert(color, row)
-    local row_str = {}
-    for _, col in pairs(row) do
-        table.insert(row_str, tostring(col))
-    end
-
-    for i, cell in pairs(row_str) do
-        if self.widths[i] == nil or #cell > self.widths[i] then
-            self.widths[i] = #cell
-        end
-    end
-
-    table.insert(self.rows, {
-        color = color,
-        cells = row_str
-    })
-end
-
-function TablePrinter:align_columns(row)
-    local padding
-    local retval = {}
-    for j, cell in pairs(row) do
-        padding = self.widths[j] - #cell
-        table.insert(retval, string.rep(" ", padding) .. " " .. cell .. " ")
-    end
-    return retval
-end
-
-function TablePrinter:colors(cell, highlight)
-    local x, y, width = table.unpack(cell)
-    if y == 0 then
-        return BLACK, WHITE
-    end
-    local bgcolor = BLACK
-    if highlight ~= nil and highlight[2] <= #self.rows then
-        local x_hit = x < highlight[1] and x + width >= highlight[1]
-        local y_hit = y == highlight[2]
-        if x_hit and y_hit then
-            bgcolor = GRAY50
-        elseif x_hit or y_hit then
-            bgcolor = GRAY30
-        end
-    end
-    return self.rows[y].color, bgcolor
-end
-
-function TablePrinter:format_row(y, highlight, row)
-    local retval = {}
-    local x = 1
-    for _, cell in pairs(self:align_columns(row.cells)) do
-        local fg, bg = self:colors({x, y, #cell}, highlight)
-        table.insert(retval, {cell, fg, bg})
-        x = x + #cell
-    end
-    return retval
-end
-
-function TablePrinter:format(highlight)
-    local retval = {}
-    table.insert(retval, self:format_row(0, highlight, self.headings))
-    for y, row in pairs(self.rows) do
-        table.insert(retval, self:format_row(y, highlight, row))
-    end
-    return retval
-end
-
 local function display_status(gpu, y, status)
-    gpu:setForeground(table.unpack(GRAY30))
-    gpu:setBackground(table.unpack(BLACK))
+    gpu:setForeground(table.unpack(colors.gray30))
+    gpu:setBackground(table.unpack(colors.black))
     gpu:setText(0, y, status)
-    gpu:setForeground(table.unpack(WHITE))
+    gpu:setForeground(table.unpack(colors.white))
 end
 
 local function display(history, highlight, gpu, status)
@@ -340,13 +248,13 @@ local function display(history, highlight, gpu, status)
             local rate_longest = history:rate_per_minute(entry:item_type(), max_rate)
             local color
             if fill_percent >= 99 then
-                color = GREEN
+                color = colors.green
             elseif fill_percent > 75 and rate_longest > 0 then
-                color = WHITE
+                color = colors.white
             elseif rate_longest > 0 then
-                color = YELLOW
+                color = colors.yellow
             else
-                color = RED
+                color = colors.red
             end
             local cells = {entry:item_type().name, entry.count, entry.storage_capacity, entry:get_fill_percent()}
             for _, rate in pairs(CONFIG.rates) do
@@ -356,31 +264,7 @@ local function display(history, highlight, gpu, status)
         end
         table_printer:sort()
 
-        local rows = table_printer:format(highlight)
-        for _, row in pairs(rows) do
-            local this_width = 0
-            for _, cell in pairs(row) do
-                this_width = this_width + #cell[1]
-            end
-            if this_width > width then
-                width = this_width
-            end
-        end
-        local height = #rows + 2
-        gpu:setSize(width, height)
-        gpu:setForeground(table.unpack(WHITE))
-        gpu:setBackground(table.unpack(BLACK))
-        gpu:fill(0, 0, width, height, "")
-        for y, row in pairs(rows) do
-            local x = 0
-            for _, cellspec in pairs(row) do
-                local cell, fg, bg = table.unpack(cellspec)
-                gpu:setForeground(table.unpack(fg))
-                gpu:setBackground(table.unpack(bg))
-                gpu:setText(x, y - 1, cell)
-                x = x + #cell
-            end
-        end
+        local height = table_printer:print(highlight, gpu)
         display_status(gpu, height - 1, status)
     else
         display_status(gpu, 0, status)
