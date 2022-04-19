f885e79b07b2e63c2434c0ac6da779ad036e0a5d
Alexander Johnston <alexanderj.roaming@gmail.com>
2022-04-19 00:33:27 -0400

    Databse self referential update

--- lib/database.lua
+++ lib/database.lua
@@ -4,16 +4,16 @@ local database_entry  = Deps("lib/database_entry")
 
 local Database = class("Database")
 function Database:initialize()
-    self.entries = {}
+    self.registry = {}
 end
 
 function Database:entry(item_type)
-    local item_type_index = item_type_registry:register(item_type)
-    if self.entries[item_type_index] == nil then
-        self.entries[item_type_index] = database_entry:new{
+    self.registry = item_type_registry:register(item_type)
+    if self.registry.entries[item_type_index] == nil then
+        self.registry.entries[item_type_index] = database_entry:new{
             item_type_index = item_type_index
         }
     end
-    return self.entries[item_type_index]
+    return self.registry.entries[item_type_index]
 end
 return Database
